b55bdc9fe36839297e8e4baa0b5cc6721195ca48
skovorodko-ov <skovorodko29081989@gmail.com>
2021-05-14 19:02:22 +0300

    added new page

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
